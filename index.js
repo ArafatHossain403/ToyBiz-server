0e9ad32dbd2ae7257dfa693b0430047cc6287be2
Arafat <arafatnubt@gmail.com>
2023-05-23 23:21:47 +0600

    vercel up

--- index.js
+++ index.js
@@ -1,42 +1,72 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const req = require('express/lib/request');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+ 
 app.use(express.json());
+const toys= require('./data/allToy.json');
+const res = require('express/lib/response');
+app.use(cors());
 
-console.log(process.env.DB_PASS)
+
+// console.log(process.env.DB_PASS)
+// console.log(process.env.DB_USER)
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ik2bdct.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ik2bdct.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-      
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+ 
+//     const addingToy = client.db('toybiz').collection('allToy');
+    
+//     app.get('/allToy', async(req, res) =>{
+//         const cursor = addingToy.find();
+//         const result= await cursor.toArray();
+//         res.send(result);
+//     })
+
+//     app.post('/allToy', async (req,res) => {
+//         const addToy = req.body;
+//         console.log(addToy);
+//         const result= await addingToy.insertOne(addToy);
+//         res.send(result);
+    
+//     })
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+app.get('/allToy', (req, res) => {
+    res.send(toys);
+})
+app.get('/allToy/:id', (req,res) =>{
+    const id= req.params.id;
+    const selectToy= toys.find(n=>n.id ==id);
+    res.send(selectToy);
+})
+  
 
 
 app.get('/', (req, res) => {
